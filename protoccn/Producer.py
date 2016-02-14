406870349b5a0d5ae87e90e6f1b8fe543a83a971
Christopher Wood <christopherwood07@gmail.com>
2016-02-14 08:59:10 -0800

    Cleanup

--- protoccn/Producer.py
+++ protoccn/Producer.py
@@ -1,3 +1,5 @@
+# import PortalWrapper
+
 class Producer(object):
     def __init__(self, name, prefix = ""):
         self.name = name
@@ -5,31 +7,13 @@ class Producer(object):
         self.handlers = {}
 
     # TODO: make prefix a regex for names
-    # TODO: if the producer has a prefix, then don't use the prefix here
-    def prefix(self, prefix, **options):
-        """A decorator that is used to register a view function for a
-        given URL rule.  This does the same thing as :meth:`add_url_rule`
-        but is intended for decorator usage::
-            @app.route('/')
-            def index():
-                return 'Hello World'
-        For more information refer to :ref:`url-route-registrations`.
-        :param rule: the URL rule as string
-        :param endpoint: the endpoint for the registered URL rule.  Flask
-                         itself assumes the name of the view function as
-                         endpoint
-        :param options: the options to be forwarded to the underlying
-                        :class:`~werkzeug.routing.Rule` object.  A change
-                        to Werkzeug is handling of method options.  methods
-                        is a list of methods this rule should be limited
-                        to (``GET``, ``POST`` etc.).  By default a rule
-                        just listens for ``GET`` (and implicitly ``HEAD``).
-                        Starting with Flask 0.6, ``OPTIONS`` is implicitly
-                        added and handled by the standard request handling.
+    def handle(self, prefix, **options):
+        """ TODO
+
+        :param prefix: TODO
+        :param options: TODO
         """
         def decorator(f):
-            #endpoint = options.pop('endpoint', None)
-            #self.add_url_rule(rule, endpoint, f, **options)
             print "setup a function: %s" % (f.__name__)
             self.handlers[prefix] = f
             return f
@@ -40,19 +24,45 @@ class Producer(object):
         pass
 
     def run(self):
-        # 1. create the portal(s)
-        # 2. jump into the listening function
+        # Create the portal
+        self.client = PortalWrapper.PortalWrapper()
+        self.client.listen(self.prefix)
+
+        # Jump into the listening function
+        while True:
+            request = self.client.receive_raw()
+            name = request.name
+
+            # Perform LPM with the handler function and invoke the right request
+            target = ""
+            for prefix in self.handlers:
+                if name.startsWith(prefix) and len(prefix) > target:
+                    target = prefix
+
+            # Pass the request to the right method
+            response = self.handlers[target](request)
+
+            # Return the response
+            self.client.reply(request.name, response)
         pass
 
     def __str__(self):
         # TODO: return a generic
         return ""
 
-app = Producer(__name__)
+app = Producer(__name__, "/foo/bar")
 
-@app.prefix("/")
-def base(message):
-    print message
+@app.handle("/baz")
+def baz(baz):
+    print baz
+    pass
 
-base("test")   
+# @app.prefix("/car")
+# def car(car):
+#     print "car!"
+#     return car
 
+baz("test") # during the run function, these functions would be invoked based on the name.
+
+# blast off
+# app.run()
